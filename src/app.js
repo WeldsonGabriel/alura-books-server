e07cb92fe67b01bcb49b122f16fb2b37b8a7c5de
Gabriel Borges <gabrielborgesw2@gmail.com>
2025-04-22 08:01:52 -0300

    "primeiro commit alura"

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require ("express")
+
+const app = express()
+
+const port = 8080
+
+app.get("/", (req, res) => {
+    res.send("Olá!")
+})
+
+app.listen(port, () => {
+    console.log("Server started on port " + port)
+})
